38bf6f63e5443283f5ff64db5194c07d4da092f8
Machi-san <maxibengochea67@gmail.com>
2024-05-19 19:13:31 -0400

    change test for example

---
+++ src/ebook_utils/example_export.py
@@ -0,0 +1,7 @@
+from book import Book, BookChapter, BookMeta, BookToc
+
+#no_request_data.keys(): publisher_name, publisher_email, subtitle
+meta = BookMeta('ALL ABOUT SEX', 'Sr Sex', 'sex123')
+pages = BookToc([BookChapter('cap1', 'sex1'), BookChapter('cap2', 'sex2'), BookChapter('cap3', 'sex3')])
+book = Book(pages, meta)
+print(book.export())
