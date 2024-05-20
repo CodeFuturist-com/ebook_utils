0c7385bde6cfaa57890686359db8f921716bd7f1
Machi-san <maxibengochea67@gmail.com>
2024-05-19 20:18:17 -0400

    add params

--- src/ebook_utils/example_export.py
+++ src/ebook_utils/example_export.py
@@ -1,7 +1,6 @@
 from book import Book, BookChapter, BookMeta, BookToc
 
-#no_request_data.keys(): publisher_name, publisher_email, subtitle
-meta = BookMeta('ALL ABOUT SEX', 'Sr Sex', 'sex123')
+meta = BookMeta('ALL ABOUT SEX', 'Sr Sex', 'sex123',subtitle='Sex Edition', email='sex@mail.com')
 pages = BookToc([BookChapter('cap1', 'sex1'), BookChapter('cap2', 'sex2'), BookChapter('cap3', 'sex3')])
 book = Book(pages, meta)
 print(book.export())
