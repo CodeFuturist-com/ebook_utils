01b1188e32a18b3de6d6c9cffa27be894a0e8d50
Machi-san <maxibengochea67@gmail.com>
2024-05-27 20:33:41 -0400

    rename to example

---
+++ src/ebook_utils/example.py
@@ -0,0 +1,12 @@
+import os
+
+from book import Book, BookChapter, BookMeta, BookToc
+from utils import rem_dir
+
+#EXPORT
+#meta = BookMeta('ALL ABOUT SEX', 'Sr Sex', 'sex123',subtitle='Sex Edition', email='sex@gmail.com')
+#pages = BookToc([BookChapter('cap1', 'sex1'), BookChapter('cap2', 'sex2'), BookChapter('cap3', 'sex3')])
+#book = Book(pages, meta)
+#print(book.export())
+
+
--- src/ebook_utils/example_export.py
+++
@@ -1,6 +0,0 @@
-from book import Book, BookChapter, BookMeta, BookToc
-
-meta = BookMeta('ALL ABOUT SEX', 'Sr Sex', 'sex123',subtitle='Sex Edition', email='sex@mail.com')
-pages = BookToc([BookChapter('cap1', 'sex1'), BookChapter('cap2', 'sex2'), BookChapter('cap3', 'sex3')])
-book = Book(pages, meta)
-print(book.export())
