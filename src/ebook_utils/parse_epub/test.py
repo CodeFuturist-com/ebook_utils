488cfb741a2dfd41e22b82cbcb53f658f6cc65d6
Machi-san <maxibengochea67@gmail.com>
2024-05-30 00:34:28 -0400

    add .txt extension

--- src/ebook_utils/parse_epub/test.py
+++ src/ebook_utils/parse_epub/test.py
@@ -7,20 +7,20 @@ rel_path = os.path.dirname(os.path.abspath(__file__))
 content_path = os.path.join(rel_path, "assets")
 res_path = os.path.join(rel_path, "parse")
 
-data = content(os.path.join(content_path, '9788026877301.epub'))
+data = content(os.path.join(content_path, '9788027244980.epub'))
 reset_folder(res_path)
 
 def print_data(data: dict, path=res_path):
   for key in data:
     if type(data[key]) != dict:
-      with open(f'{path}/{key}', 'w') as f:
+      with open(f'{path}/{key}.txt', 'w') as f:
         f.write(f'CONTENT:\n{data[key]}')
     
     else:
       create_folder(f'{path}/{key}')
       print_data(data[key], f'{path}/{key}')
       
-#print_data(data)
+print_data(data)
 
 
     
