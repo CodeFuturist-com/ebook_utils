b2d47ebd05c110c65f89155e77c9fdb30f7523bf
adrianhdez929 <adrianhdez929@gmail.com>
2024-06-04 10:52:52 -0400

    fix nested book tocs structure

--- src/ebook_utils/book.py
+++ src/ebook_utils/book.py
@@ -6,106 +6,8 @@ from templates import *
 #metodos auxiliares
 from utils import create_folder, compress, rem_dir, check_epub, unzip, dir_toc, p_group, p_content, find_root_folder, epub_id
 
-class BookMeta:
-    def __init__(self, title: str, author: str, ean: str, subtitle='', publisher='', email=''):
-        self._meta = {
-            'title': title,
-            'author': author,
-            'ean': ean,
-            'subtitle': subtitle,
-            'publisher': publisher,
-            'email': email
-        }
 
-    def __getitem__(self, key):
-        return self._meta[key]
-
-class BookChapter:
-    def __init__(self, title: str, content: str) -> None:
-        self._title = title
-        self._content = content
-
-    @property
-    def title(self) -> str:
-        return self._title
-
-    @property
-    def content(self) -> str:
-        return self._content
-    
-    def __str__(self) -> str:
-        return self.title
-
-class BookToc:
-    def __init__(self, title: str, contents: list) -> None:
-        self._title = title
-        pages = []
-        tocs = []
-
-        for item in contents:
-            # If the type is BookToc add it to tocs list
-            if isinstance(item, self.__class__):
-                tocs.append(item)
-            else:
-                pages.append(item)
-           
-        self._pages = pages
-        self._tocs = tocs
-
-    # TODO: Implement the book parsing here
-    @classmethod
-    def from_book(cls, book):
-       contents = cls._content(book)
-       return cls('Sex', contents)   
-    
-    #dado un epub, parsear el contenido
-    @classmethod
-    def _content(cls, epub: str) -> list:
-      unzip(epub) #descomprimir el epub
-      absolute_toc = dir_toc(epub) #guardar la direccion de la toc del epub
-      result = cls._content_rec(epub, [], absolute_toc, absolute_toc) #respuesta
-      rem_dir([f"{epub.replace('.epub', '')}"]) #borrar el descomprimido luego del parsing
-      return result 
-    
-    #metodo recursivo para las collections
-    @classmethod
-    def _content_rec(cls, epub: str, result: list, absolute_toc: str, parents_tocs: str, link_path=None) -> list:
-      data_toc = cls._child_text(epub, link_path, absolute_toc, parents_tocs) #por cada titulo, el path de lo que apunta cada link
-      values = list(data_toc.values()) #valores del diccionario para preguntar por el id siguiente
-      i = 0 #inicializar el iterador
-
-      for key in data_toc:
-        with open(data_toc[key][0], 'r') as f:
-          doc = BeautifulSoup(f, 'xml')
-
-          #si tiene mas de un tag 'a' y no tiene tags 'p' es una toc
-          if len(doc.find_all('a')) > 1 and doc.find('p') == None:
-            content = cls._content_rec(epub, [], absolute_toc, f"{parents_tocs}|{data_toc[key][0].split('/')[-1]}", data_toc[key][0])
-
-            if len(content) != 0:
-              result.append(BookToc(key, content))
-
-          #si hay 2 path iguales consecutivos, el title referencia a un id
-          elif i < len(data_toc) - 1 and data_toc[key][0] == values[i + 1][0]:
-            content = cls._content_chapter(data_toc[key], values[i + 1][1])
-
-            if content != '':
-              result.append(BookChapter(key, content))
-
-          #en cualquier oto caso, dame todos los p
-          else:
-            content = cls._content_chapter(data_toc[key])
-
-            if content != '':
-              result.append(BookChapter(key, content))
-
-        i += 1
-
-      return result
-    
-    #guardar por cada title, la direccion de los capitulos
-    @classmethod
-    def _child_text(cls, epub: str, toc, path_toc: str, parents_tocs: str) -> dict:
+def _child_text(epub: str, toc, path_toc: str, parents_tocs: str) -> dict:
       result = {} #respuesta
       root_folder = find_root_folder(f"{epub.replace('.epub', '')}") #carpeta raiz de los textos
       dir = f"{epub.replace('.epub', '')}/{root_folder}" #inicializar el directorio y la toc
@@ -138,10 +40,8 @@ class BookToc:
             result[f'{tag.text}'] = epub_id(tag['href'], dir)
 
       return result
-    
-    #dado el nombre del capitulo extraer el texto correspondiente
-    @classmethod
-    def _content_chapter(cls, chapter: tuple[str, str], id_end=None) -> str:
+
+def _content_chapter(chapter: tuple[str, str], id_end=None) -> str:
       with open(chapter[0], 'r') as f:
         doc = BeautifulSoup(f, 'xml')
 
@@ -152,24 +52,162 @@ class BookToc:
         #dame todos los 'p' del directorio
         return p_group(p_content(doc))
 
+
+def _content_rec(epub: str, result: list, absolute_toc: str, parents_tocs: str, link_path=None) -> list:
+      data_toc = _child_text(epub, link_path, absolute_toc, parents_tocs) #por cada titulo, el path de lo que apunta cada link
+      values = list(data_toc.values()) #valores del diccionario para preguntar por el id siguiente
+      i = 0 #inicializar el iterador
+
+      for key in data_toc:
+        with open(data_toc[key][0], 'r') as f:
+          doc = BeautifulSoup(f, 'xml')
+
+          #si tiene mas de un tag 'a' y no tiene tags 'p' es una toc
+          if len(doc.find_all('a')) > 1 and doc.find('p') == None:
+            content = _content_rec(epub, [], absolute_toc, f"{parents_tocs}|{data_toc[key][0].split('/')[-1]}", data_toc[key][0])
+
+            if len(content) != 0:
+              result.append(BookToc(key, content))
+
+          #si hay 2 path iguales consecutivos, el title referencia a un id
+          elif i < len(data_toc) - 1 and data_toc[key][0] == values[i + 1][0]:
+            content = _content_chapter(data_toc[key], values[i + 1][1])
+
+            if content != '':
+              result.append(BookChapter(key, content))
+
+          #en cualquier oto caso, dame todos los p
+          else:
+            content = _content_chapter(data_toc[key])
+
+            if content != '':
+              result.append(BookChapter(key, content))
+
+        i += 1
+
+      return result
+
+def _content(epub: str) -> list:
+      unzip(epub) #descomprimir el epub
+      absolute_toc = dir_toc(epub) #guardar la direccion de la toc del epub
+      result = _content_rec(epub, [], absolute_toc, absolute_toc) #respuesta
+      rem_dir([f"{epub.replace('.epub', '')}"]) #borrar el descomprimido luego del parsing
+      return result 
+
+def is_toc(page: any) -> bool:
+    if isinstance(page, BookToc): return True
+    return False
+
+
+
+class BookMeta:
+    def __init__(self, title: str, author: str, ean: str, subtitle='', publisher='', email=''):
+        self._meta = {
+            'title': title,
+            'author': author,
+            'ean': ean,
+            'subtitle': subtitle,
+            'publisher': publisher,
+            'email': email
+        }
+
+    def __getitem__(self, key):
+        return self._meta[key]
+
+class BookChapter:
+    def __init__(self, title: str, content: str) -> None:
+        self._title = title
+        self._content = content.replace('&', '&amp;')
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def content(self) -> str:
+        return self._content
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    @property
+    def ref(self) -> str:
+       return self._ref
+    
+    def generate(self, meta: BookMeta, index: int) -> int:
+        with open(f'my_epub/OEBPS/Text/part000{index}.xhtml', 'w') as f:
+          self._ref = f'part000{index}.xhtml'
+          f.write(CHAPTER.format(meta['title'], self.title, self.content))
+        return index
+
+class BookToc:
+    def __init__(self, title: str, contents: list) -> None:
+        self._title = title
+        self._contents = contents
+
+    @classmethod
+    def from_book(cls, book):
+       contents = _content(book)
+       return cls('Sex', contents)   
+    
+
     # Returns the plain page list of toc recursively
     @property
     def pages(self):
         page_list = []
-        for x in self._tocs:
-          page_list = page_list + x.pages
-        return page_list + self._pages
+
+        for item in self._contents:
+            if is_toc(item):
+                page_list = page_list + item.pages
+            else:
+                page_list.append(item)
+        return page_list
     
     @property
     def title(self):
        return self._title
 
     @property
-    def tocs(self):
-        return self._tocs
+    def tocs_count(self):
+        count = 1
+        for page in self._contents:
+           if is_toc(page):
+              count += page.tocs_count
+        return count
     
     def __str__(self) -> str:
        return f"{self.title}"
+    
+    def generate(self, meta: BookMeta, index: str, is_main=False) -> int:
+        l_index = index
+
+        if is_main:
+            with open('my_epub/OEBPS/Text/contents.xhtml', 'w') as f:
+                links = ''
+              
+                for page in self._contents:
+                    links += TocLink(l_index + 1, page.title).content
+
+                    if is_toc(page):
+                        l_index = page.generate(meta, l_index + 1)
+                    else:
+                        l_index = page.generate(meta, l_index + 1)
+                    
+                f.write(TOC.format(links))
+        else:
+            with open(f'my_epub/OEBPS/Text/part000{l_index}.xhtml', 'w') as f:
+                links = ''
+              
+                for page in self._contents:
+                    links += TocLink(l_index + 1, page.title).content
+
+                    if is_toc(page):
+                        l_index = page.generate(meta, l_index + 1)
+                    else:
+                        l_index = page.generate(meta, l_index + 1)
+                    
+                f.write(NESTED_TOC.format(page.title, links))
+        return l_index
 
 #objeto para manejar los navpoints de toc.ncx
 class NavPoint:
@@ -184,7 +222,7 @@ class NavPoint:
     result += '<navLabel>\n'
     result += f'<text>{self._page.title}</text>\n'
     result += '</navLabel>\n'
-    result += f'<content src="Text/part000{self._index - 3}.xhtml"/>\n'
+    result += f'<content src="Text/{self._page.ref}"/>\n'
     result += '</navPoint>\n'
     return result
     
@@ -204,7 +242,7 @@ class TocLink:
     result += '<div class="sgc-toc-level-1 sgc-1">\n'
     result += f' <a href="part000{self._index}.xhtml">{self._title}</a>\n'
     result += '</div>\n'
-    return result
+    return result.replace('&', '&amp;')
     
   @property
   def content(self) -> str:
@@ -220,9 +258,9 @@ class Book:
     self._gen_static_structure()
     
     #estructura variable
+    self._gen_text()
     self._gen_toc_ncx()
     self._gen_content()
-    self._gen_text()
   
     #comprimir el epub, borrar el descomprimido y chequear el epub
     compress('my_epub', self._meta['ean'], '.epub')
@@ -264,8 +302,7 @@ class Book:
     result = ''
     i = 3
 
-    # TODO: esto hay que cambiarlo a la nueva estructura recursiva de BookToc
-    for page in self._toc._pages:
+    for page in self._toc.pages:
       result += NavPoint(page, i).content
       i += 1
 
@@ -285,7 +322,7 @@ class Book:
     result += '<item id="frontpage.xhtml" href="Text/frontpage.xhtml" media-type="application/xhtml+xml"/>\n'
     result += '<item id="contents.xhtml" href="Text/contents.xhtml" media-type="application/xhtml+xml"/>\n'
 
-    for i in range(len(self._toc._pages)):
+    for i in range(1, len(self._toc.pages) + self._toc.tocs_count):
       result += f'<item id="section{i}.xhtml" href="Text/part000{i}.xhtml" media-type="application/xhtml+xml"/>\n'
 
     #agregar la referencia al css
@@ -300,16 +337,16 @@ class Book:
     result += '<itemref idref="frontpage.xhtml"/>\n'
     result += '<itemref idref="contents.xhtml"/>\n'
 
-    # TODO: adaptarlo a la nueva estructura recursiva de BookToc
-    for i in range(len(self._toc._pages)):
+    for i in range(1, len(self._toc.pages) + self._toc.tocs_count):
       result += f'<itemref idref="section{i}.xhtml"/>\n'
 
     return result
   
   #generar carpeta text
   def _gen_text(self):
+    self._toc.generate(self._meta, 0, True)
     self._gen_front_toc()
-    self._gen_chapters()
+    # self._gen_chapters()
     
   #generar la presentacion del libro y el toc
   def _gen_front_toc(self):
@@ -319,21 +356,4 @@ class Book:
                                '' if self._meta['email'] == '' else 'Contact: ',self._meta['email'], self._meta['ean'])) 
                               
     #crear la toc
-    with open('my_epub/OEBPS/Text/contents.xhtml', 'w') as f:
-      links = ''
-      
-      # TODO: adaptarlo a la nueva estructura recursiva de BookToc
-      for i in range(len(self._toc._pages)):
-        links += TocLink(i, self._toc[i].title).content
-        
-      f.write(TOC.format(links))
-
-  #generar los capitulos
-  def _gen_chapters(self):
-      i = 0
-
-      for page in self._toc._pages:  
-        with open(f'my_epub/OEBPS/Text/part000{i}.xhtml', 'w') as f:
-          f.write(CHAPTER.format(self._meta['title'], page.title, page.content))
-          
-        i += 1
+    
