47da40955480605f307279e8de181d1b14310f22
Yandi Vargas <yandivd@gmail.com>
2024-06-24 13:42:48 -0400

    function created to generaate audiobook from epub with params

--- index.py
+++ index.py
@@ -1,24 +1,17 @@
 from models import BookProduction
 from src.ebook_utils.book import Book
 
-book_inst = Book.from_book('4064066459796.epub')
+def generate_audiobook(book_path, narrator, output_path, description, model, voice, api_key):
+    book_inst = Book.from_book(book_path)
 
-# book_prod = BookProduction(title='La Dama y el Vagabundo', subtitle='Bienvenido', author='Yandi Vargas', narrator='Libni Borrego', output_path='output/', description='Ease una vez...')
-# book_prod.add_chapter("Chapter Title", "Chapter Content")
-# book_prod.generate_voices(model="tts-1-hd", voice="alloy")
-# book_prod.process_audio_files()
-# Crear una instancia de BookProduction para el libro importado
-book_prod = BookProduction(title=book_inst.title, subtitle=book_inst.subtitle, 
-                           author=book_inst.author, narrator='Libni Borrego', 
-                           output_path='output/', description='SSSSSSSSSSSSSS')
+    book_prod = BookProduction(title=book_inst.title, subtitle=book_inst.subtitle, 
+                               author=book_inst.author, narrator=narrator, 
+                               output_path=output_path, description=description)
 
-# Iterar sobre los capítulos del libro importado
-for chapter in book_inst.chapters:
-    # Agregar cada capítulo a la instancia de BookProduction
-    book_prod.add_chapter(chapter.title, chapter.content)
+    for chapter in book_inst.chapters:
+        book_prod.add_chapter(chapter.title, chapter.content)
 
-# Generar voces para los capítulos
-book_prod.generate_voices(model="tts-1-hd", voice="alloy", api_key='')
+    book_prod.generate_voices(model=model, voice=voice, api_key=api_key)
+    book_prod.process_audio_files()
 
-# Procesar archivos de audio para los capítulos
-book_prod.process_audio_files()
+generate_audiobook(book_path='test.epub', narrator='Yandi Vargas Domínguez', output_path='output/', description='', model='tts-1-hd', voice='alloy', api_key='')
